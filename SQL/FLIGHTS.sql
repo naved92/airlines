3761ba9ce4b71a97449cf2ed6a577a304e287e32
naved92 <imtiaznaved@gmail.com>
2015-05-18 05:13:41 +0600

    SQL Flies

--- SQL/FLIGHTS.sql
+++ SQL/FLIGHTS.sql
@@ -1,14 +1,14 @@
 CREATE TABLE FLIGHTS(
-Flight_code int PRIMARY KEY,
-Source_airport_id int ,
-Destination_airport_id int,
-Depart_time TIMESTAMP ,
-Arrival_time TIMESTAMP,
-Distance INT,
-Constraint fk_src_air FOREIGN KEY(Source_airport_id) 
-REFERENCES AIRPORTS(Airport_code),
-Constraint fk_dest_air FOREIGN KEY (Destination_airport_id)
-REFERENCES AIRPORTS(Airport_code)
+FLIGHT_CODE int PRIMARY KEY,
+SOURCE_AIRPORT_ID int ,
+DESTINATION_AIRPORT_ID int,
+DEPART_TIME TIMESTAMP ,
+ARRIVAL_TIME TIMESTAMP,
+DISTANCE INT,
+Constraint fk_src_air FOREIGN KEY(SOURCE_AIRPORT_ID) 
+REFERENCES AIRPORTS(AIRPORT_CODE),
+Constraint fk_dest_air FOREIGN KEY (DESTINATION_AIRPORT_ID)
+REFERENCES AIRPORTS(AIRPORT_CODE)
 );
 
-CREATE SEQUENCE seq_flight_code;
+CREATE SEQUENCE SEQ_FLIGHT_CODE;
